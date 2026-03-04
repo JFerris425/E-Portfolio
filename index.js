5ec09d356486e32c7fe3d2798ca2997e0f5a2ad1
JFerris425 <jferris42507@gmail.com>
2026-03-03 22:25:12 -0500

    10:25pm 3/3/2026

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 function contact(event) {
-    event.preventDefault()
-    emailjs
-        .sendForm(
-            'service_i5f5sir',
-            'template_3ktf1rw',
-            event.target,
-            'Ir5ArnzeYtZIcZewh'
-        ).then(() => {
-            console.log('this worked1')
-        })
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_i5f5sir",
+      "template_3ktf1rw",
+      event.target,
+      "Ir5ArnzeYtZIcZewh",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at ferris.sites@gmail.com",
+      );
+    });
+}
+
+let isModalOpen = false
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+
+    document.body.classList += " modal--open"
 }
